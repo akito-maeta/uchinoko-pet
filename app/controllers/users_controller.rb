9e4e483c85ed33d844aae631e472557bc59206c9
vagrant <nervous6622@gmail.com>
2020-09-22 08:12:07 +0000

    url-name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
 def show
-    @user = User.find(params[:id])
+    @user = User.find_by(name: params[:name])
     @posts = @user.posts
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find_by(name: params[:name])
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find_by(name: params[:name])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts
-  resources :users, only: [:show, :edit, :update]
+  resources :users,param: :name, path: '/', only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
