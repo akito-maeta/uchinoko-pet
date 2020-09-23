ec49d350d8df51dd32ad15799cc9c0c66f066796
vagrant <nervous6622@gmail.com>
2020-09-23 08:23:19 +0000

    [add]fav-demo-template

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: @post.id)
+    like = @post.likes.new(user_id: current_user.id)
     like.save
     redirect_to request.referer
   end
